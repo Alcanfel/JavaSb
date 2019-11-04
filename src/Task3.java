5c0a1d78fd47634783f7d2aa97c0642cb5c6afb0
attenuator <artur_neo@mail.ru>
2019-11-04 11:44:34 +0500

    add method

--- src/Task3.java
+++ src/Task3.java
@@ -1,13 +1,20 @@
 class Task3 {
     public static void main(String[] args) {
         String s = new String("Программирование это хорошо, но есть что-то лучше");
-        char ch = 'н';
+        char ch = 'о';
         int col = getCountChar(s, ch);
-        System.out.format("Исходная строка: %s\nСимвол поиска: %c\nКоличество раз - %d, которое встречается данный символ\n", s, ch, col);
+        //1 вариант
+        System.out.format("Вариант№1\nИсходная строка: %s\nСимвол поиска: %c\nКоличество раз - %d, которое встречается данный символ\n\n", s, ch, col);
 
-        StringBuilder positionString = getPositionChar(s, ch);
-        System.out.println(positionString);
+         //2 вариант
+        int col2 = getCountCharV2(s ,ch);
+        System.out.format("Вариант№2\nИсходная строка: %s\nСимвол поиска: %c\nКоличество раз - %d, которое встречается данный символ\n\n", s, ch, col2);
+        System.out.format("Позиции: %s \n",getPositionChar(s, ch));
+
+
     }
+
+
 
     // 1 - Количество раз, которое встречается данный символ
     public static int getCountChar(String s, char ch) {
@@ -21,11 +28,18 @@ class Task3 {
         return countChar;
     }
 
+    // Еще один вариант реализации
+    public static int getCountCharV2(String s, char ch) {
+        String[] n = s.split(String.valueOf(ch));
+        int result = n.length-1;
+        return result;
+    }
+
     // 2 - Дополнительное задание - вывести позиции этих символов
 
     public static StringBuilder getPositionChar(String s, char ch) {
         char[] arrayChar = s.toCharArray();
-        StringBuilder positionString = new StringBuilder("Позиции:");
+        StringBuilder positionString = new StringBuilder();
         for (int i = 0; i < arrayChar.length; i++) {
             if (arrayChar[i] == ch) {
                 positionString.append(i+1).append(" ");
--- src/Task4.java
+++ src/Task4.java
@@ -1,16 +1,17 @@
 import javax.sound.midi.Soundbank;
+import java.util.Stack;
 
 class Task4{
     public static void main(String[] args){
-        System.out.println(simpleCalculate("2 + 2"));
-        System.out.println(simpleCalculate("2 * 2"));
-        System.out.println(simpleCalculate("2 / 2"));
-        System.out.println(simpleCalculate("2 - 2"));
-        System.out.println(hardCalculate("2 + 2 / 3 * 7"));
+        simpleCalculate("2 + 2");
+        simpleCalculate("2 * 6");
+        simpleCalculate("8 / 2");
+        simpleCalculate("10 - 2");
+        hardCalculate("12 + 2 / 3 * 7");
 
     }
 
-    public static int simpleCalculate(String s) {
+    public static void simpleCalculate(String s) {
         String[] arrayStr = s.split(" ");
         int result = 0;
         switch (arrayStr[1]) {
@@ -20,11 +21,21 @@ class Task4{
             case "-": result+=Integer.parseInt(arrayStr[0])-Integer.parseInt(arrayStr[2]); break;
             default: result = 0; break;
         }
-        return result;
+        System.out.format("Результат операции %s %s %s = %d\n", arrayStr[0],arrayStr[1], arrayStr[2], result);
 
 
     }
-    public static int hardCalculate(String s){
+    public static void hardCalculate(String s){
+
+        char[] arrayStr = s.replaceAll(" ", "").toCharArray();
+        Stack<Integer> stack = new Stack<>();
+        for (char temp: arrayStr
+             ) {
+            System.out.println(temp);
+
+        }
+
+
 //        String[] arrayStr = s.split(" ");
 //        for (String temp: arrayStr){
 //            System.out.println(temp);
@@ -46,7 +57,5 @@ class Task4{
 //            }
 //        }
 //
-//        System.out.println(strres);
-        return 0;
     }
 }
