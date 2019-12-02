e3131b138f2f94648afb9ad9402fb86b7e7ef756
attenuator <artur_neo@mail.ru>
2019-12-02 18:33:40 +0500

    add

--- src/Task1419/Task5/Main.java
+++ src/Task1419/Task5/Main.java
@@ -1,6 +1,9 @@
 package Task1419.Task5;
 
 import javax.naming.spi.ObjectFactoryBuilder;
+import java.io.ObjectStreamException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,10 +12,11 @@ public class Main {
         Map<Object, Object> map = new HashMap<>();
         map.put(1, "100");
         map.put(2, "200");
+        map.put(5, "200");
         map.put(3, "300");
-        map.put(4, "400");
+        map.put(4, "300");
 
-        Map<Object, Object> newMap = reverse(map);
+        Map<Object, Collection<Object> > newMap = reverse(map);
 
         for (Object temp: newMap.keySet()
              ) {
@@ -21,12 +25,21 @@ public class Main {
         
     }
 
-    public static Map<Object, Object> reverse(Map<Object, Object> map){
-        Map<Object, Object> newMap = new HashMap<>();
+    public static Map<Object, Collection<Object> > reverse(Map<Object, Object> map){
+        Map<Object, Collection<Object>> newMap = new HashMap<>();
 
         for (Object key: map.keySet()
              ) {
-            newMap.put(map.get(key), key);
+            Collection<Object> newList = new ArrayList<>();
+            if (!newMap.containsKey(map.get(key))){
+                newList.add(key);
+                newMap.put(map.get(key), newList);
+            } else{
+                Collection<Object> oldlist = (Collection<Object>) newMap.get(map.get(key));
+                oldlist.add(key);
+                newMap.put(map.get(key), oldlist);
+            }
+
         }
         return newMap;
     }
